99f99f69afd2eeaf1594c5eaa1f3149a27412273
Panagiotis Milios <Razer_greece@yahoo.gr>
2021-03-11 10:41:24 +0200

    v5

--- index.js
+++ index.js
@@ -43,104 +43,107 @@ $(document).ready(function() {
 
 $(document).ready(function() {
 	//$('#breadcrumb').append("<h2> Home </h2>");
+	//Main Directory Setups
+	basePathSetups = "setups/";
+	bit32Path = "32-bit/";
+	bit64Path = "64-bit/";
+
+	//Main Directory Photos
+	basePathPhotos = "photos/";
+
+	//Setups των εφαρμογών 
+
+	//.net framework
+	var pathNetFramework = "ChromeSetup.exe"; //Setup name
+	var netFrameworkImg = "net_framework.png"; //Photo name
+	var netFrameworkdDesc = ".Net Framework"; //Description name
+
+	//Driverpack 
+	var pathDriverpack = "ChromeSetup.exe";
+	var driverpackImg = "Driver_pack_solution.jpg";
+	var driverpackDesc = "Driverpack Solution";
+
+	//Greek Language
+	var pathGreekLanguage = "ChromeSetup.exe";
+	var greekLanguageImg = "greek_language.png";
+	var greekLanguageDesc = "Windows Greek Language";
+
+	//KMSpico
+	var pathKmspico = "ChromeSetup.exe";
+	var kmspicoImg = "km_spico.png";
+	var kmspicoDesc = "KMSpico";
+
+	//Chrome
+	var pathChrome = "ChromeSetup.exe";
+	var chromeSetupImg = "chrome.jpg";
+	var chromeDesc = "Chrome Browser";
+
+	//VLC
+	var pathVLC = "ChromeSetup.exe";
+	var vlcImg = "vlc.jpg";
+	var vlcDesc = "VLC Media Player";
+
+	//Winrar
+	var pathWinrar = "ChromeSetup.exe";
+	var winrarImg = "winrar.jpg";
+	var winrarDesc = "Winrar";
+
+	//ESET ANTIVIRUS
+	var pathEset = "ChromeSetup.exe";
+	var esetImg = "eset.jpg";
+	var esetDesc = "Eset Smart Security Antivirus";
+
+	//Radmin
+	var pathRadmin = "ChromeSetup.exe";
+	var radminImg = "radmin.jpg";
+	var radminDesc = "Radmin";
+
+	//Radmin Crack
+	var pathRadminCrack = "ChromeSetup.exe";
+	var radmincrackImg = "radmin_crack.png";
+	var radminCrackDesc = "Radmin Crack";
+
+	//Office
+	var pathOffice = "ChromeSetup.exe";
+	var officeImg = "office.png";
+	var officeDesc = "Microsoft Office";
 	
+	//Office Greek
+	var pathOfficeGreek = "ChromeSetup.exe";
+	var officeGreekImg = "officeGreek.png";
+	var officeGreekDesc = "Microsoft Office Greek";
+
+	//Firefox
+	var pathFirefox = "ChromeSetup.exe";
+	var firefoxImg = "firefox.jpg";
+	var firefoxDesc = "Firefox";
+	
+	//Acrobat Adobe
+	var pathAcrobat = "ChromeSetup.exe";
+	var AcrobatImg = "acrobat.jpg";
+	var AcrobatDesc = "Adobe Acrobat";
+
+	//Nitro
+	var pathNitro = "ChromeSetup.exe";
+	var nitroImg = "nitro.png";
+	var nitroDesc = "Nitro PDF";
+
+	//Nitro Keygen
+	var pathNitroKeygen = "ChromeSetup.exe";
+	var nitroKeygenImg = "nitro_keygen.jpg";
+	var nitroKeygenDesc = "Nitro Keygen";
+		
+	//windows server activator
+	var pathActivator = "ChromeSetup.exe";
+	var activatorImg = "unlocked.png";
+	var activatorDesc = "Windows Server Activator";
+	
+	
+	var successImg = "nitro_keygen.jpg";
+		
     $('.dropdown-item').on("click", function() {
         //Καθαρίζουμε την λίστα πριν την ξαναγεμίσουμε 
        // clearTitleList();
-		
-
-        //Main Directory Setups
-        basePathSetups = "setups/";
-        bit32Path = "32-bit/";
-        bit64Path = "64-bit/";
-
-        //Main Directory Photos
-        basePathPhotos = "photos/";
-
-        //Setups των εφαρμογών 
-
-        //.net framework
-        var pathNetFramework = "ChromeSetup.exe"; //Setup name
-        var netFrameworkImg = "net_framework.png"; //Photo name
-        var netFrameworkdDesc = ".Net Framework"; //Description name
-
-        //Driverpack 
-        var pathDriverpack = "ChromeSetup.exe";
-        var driverpackImg = "Driver_pack_solution.jpg";
-        var driverpackDesc = "Driverpack Solution";
-
-        //Greek Language
-        var pathGreekLanguage = "ChromeSetup.exe";
-        var greekLanguageImg = "greek_language.png";
-        var greekLanguageDesc = "Windows Greek Language";
-
-        //KMSpico
-        var pathKmspico = "ChromeSetup.exe";
-        var kmspicoImg = "km_spico.png";
-        var kmspicoDesc = "KMSpico";
-
-        //Chrome
-        var pathChrome = "ChromeSetup.exe";
-        var chromeSetupImg = "chrome.jpg";
-        var chromeDesc = "Chrome Browser";
-
-        //VLC
-        var pathVLC = "ChromeSetup.exe";
-        var vlcImg = "vlc.jpg";
-        var vlcDesc = "VLC Media Player";
-
-        //Winrar
-        var pathWinrar = "ChromeSetup.exe";
-        var winrarImg = "winrar.jpg";
-        var winrarDesc = "Winrar";
-
-        //ESET ANTIVIRUS
-        var pathEset = "ChromeSetup.exe";
-        var esetImg = "eset.jpg";
-        var esetDesc = "Eset Smart Security Antivirus";
-
-        //Radmin
-        var pathRadmin = "ChromeSetup.exe";
-        var radminImg = "radmin.jpg";
-        var radminDesc = "Radmin";
-
-        //Radmin Crack
-        var pathRadminCrack = "ChromeSetup.exe";
-        var radmincrackImg = "radmin_crack.png";
-        var radminCrackDesc = "Radmin Crack";
-
-        //Office
-        var pathOffice = "ChromeSetup.exe";
-        var officeImg = "office.png";
-        var officeDesc = "Microsoft Office";
-		
-		//Office Greek
-        var pathOfficeGreek = "ChromeSetup.exe";
-        var officeGreekImg = "officeGreek.png";
-        var officeGreekDesc = "Microsoft Office Greek";
-
-        //Firefox
-        var pathFirefox = "ChromeSetup.exe";
-        var firefoxImg = "firefox.jpg";
-        var firefoxDesc = "Firefox";
-		
-		//Acrobat Adobe
-        var pathAcrobat = "ChromeSetup.exe";
-        var AcrobatImg = "acrobat.jpg";
-        var AcrobatDesc = "Adobe Acrobat";
-
-        //Nitro
-        var pathNitro = "ChromeSetup.exe";
-        var nitroImg = "nitro.png";
-        var nitroDesc = "Nitro PDF";
-
-        //Nitro Keygen
-        var pathNitroKeygen = "ChromeSetup.exe";
-        var nitroKeygenImg = "nitro_keygen.jpg";
-        var nitroKeygenDesc = "Nitro Keygen";
-
-        var successImg = "nitro_keygen.jpg";
-		
 		
         //TODO: ALLAXE TA PATHS TWN EXE 
         if ($(this).is('#Windows7_32bit')){
@@ -324,22 +327,16 @@ $(document).ready(function() {
 		$('#breadcrumb').append("<h2>Windows Server</h2>");
 		$('#Programs').append("<div class='\container fadeIn'\><ul class='\list-group list-group-horizontal row'\>" +
 		"<li class=\'list-group-item\'><a href=\'" + basePathSetups + bit64Path + pathDriverpack + "\'><div class=\'image-wrapper\'><img src=\'" + basePathPhotos + driverpackImg + "\'></img></div><div class=\'descr\'>" + driverpackDesc + "</div></a></li>" +
-		"<li class=\'list-group-item\'><a href=\'" + basePathSetups + bit64Path + pathOffice + "\'><div class=\'image-wrapper\'><img src=\'" + basePathPhotos + officeImg + "\'></img></div><div class=\'descr\'>" + officeDesc + "</div></a></li>" +
-		"<li class=\'list-group-item\'><a href=\'" + basePathSetups + bit64Path + pathOfficeGreek + "\'><div class=\'image-wrapper\'><img src=\'" + basePathPhotos + officeGreekImg + "\'></img></div><div class=\'descr\'>" + officeGreekDesc + "</div></a></li>" +
-		"<li class=\'list-group-item\'><a href=\'" + basePathSetups + bit64Path + pathKmspico + "\'><div class=\'image-wrapper\'><img src=\'" + basePathPhotos + kmspicoImg + "\'></img></div><div class=\'descr\'>" + kmspicoDesc + "</div></a></li>" +  
-		"<li class=\'list-group-item\'><a href=\'" + basePathSetups + bit64Path + pathChrome + "\'><div class=\'image-wrapper\'><img src=\'" + basePathPhotos + chromeSetupImg + "\'></img></div><div class=\'descr\'>" + chromeDesc + "</div></a></li>" +
-		"<li class=\'list-group-item\'><a href=\'" + basePathSetups + bit64Path + pathVLC + "\'><div class=\'image-wrapper\'><img src=\'" + basePathPhotos + vlcImg + "\'></img></div><div class=\'descr\'>" + vlcDesc + "</div></a></li>" +
 		"<li class=\'list-group-item\'><a href=\'" + basePathSetups + bit64Path + pathWinrar + "\'><div class=\'image-wrapper\'><img src=\'" + basePathPhotos + winrarImg + "\'></img></div><div class=\'descr\'>" + winrarDesc + "</div></a></li>" +
 		"<li class=\'list-group-item\'><a href=\'" + basePathSetups + bit64Path + pathFirefox + "\'><div class=\'image-wrapper\'><img src=\'" + basePathPhotos + firefoxImg + "\'></img></div><div class=\'descr\'>" + firefoxDesc + "</div></a></li>" +
-		"<li class=\'list-group-item\'><a href=\'" + basePathSetups + bit64Path + pathAcrobat + "\'><div class=\'image-wrapper\'><img src=\'" + basePathPhotos + AcrobatImg + "\'></img></div><div class=\'descr\'>" + AcrobatDesc + "</div></a></li>" +
-		"<li class=\'list-group-item\'><a href=\'" + basePathSetups + bit64Path + pathNitro + "\'><div class=\'image-wrapper\'><img src=\'" + basePathPhotos + nitroImg + "\'></img></div><div class=\'descr\'>" + nitroDesc + "</div></a></li>" +
-		"<li class=\'list-group-item\'><a href=\'" + basePathSetups + bit64Path + pathNitroKeygen + "\'><div class=\'image-wrapper\'><img src=\'" + basePathPhotos + nitroKeygenImg + "\'></img></div><div class=\'descr\'>" + nitroKeygenDesc + "</div></a></li>" +
+        "<li class=\'list-group-item\'><a href=\'" + basePathSetups + bit64Path + pathRadmin + "\'><div class=\'image-wrapper\'><img src=\'" + basePathPhotos + radminImg + "\'></img></div><div class=\'descr\'>" + radminDesc + "</div></a></li>" +		
+		"<li class=\'list-group-item\'><a href=\'" + basePathSetups + bit32Path + pathEset + "\'><div class=\'image-wrapper\'><img src=\'" + basePathPhotos + esetImg + "\'></img></div><div class=\'descr\'>" + esetDesc + "</div></a></li>" +
+		"<li class=\'list-group-item\'><a href=\'" + basePathSetups + bit32Path + pathActivator + "\'><div class=\'image-wrapper\'><img src=\'" + basePathPhotos + activatorImg + "\'></img></div><div class=\'descr\'>" + activatorDesc + "</div></a></li>" +		
 		"<li class=\'list-group-item hidden-check-element\'><a href=\'" + basePathSetups + bit64Path + pathNitroKeygen + "\'><div class=\'image-wrapper\'><img src=\'photos/checktransparent.png\'></img></div><div class=\'descr\'>" + nitroKeygenDesc + "</div></a></li>" +
 		"</ul></div>");
 	});
-});
-  
-
+}); 
+	
 $(document).ready(function(e) {  
   $('.list-group-item a').on("click", function(e) {
     $(this).css('opacity' , '0.5');
